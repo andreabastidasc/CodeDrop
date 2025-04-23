f5211c2c308c62e3dc30133d3c3bb63fae595531
andreabastidasc <andrea.bcolina@gmail.com>
2025-04-22 22:36:35 -0300

    add base editor

--- client/src/components/molecules/CodeEditor/CodeEditor.tsx
+++ client/src/components/molecules/CodeEditor/CodeEditor.tsx
@@ -1,26 +1,60 @@
-import MonacoEditor from "react-monaco-editor";
+import { useEffect } from "react";
+import Editor, { loader } from "@monaco-editor/react";
+import * as monacoEditor from "monaco-editor";
+
+import monokai from "monaco-themes/themes/Monokai.json";
+import chrome from "monaco-themes/themes/Chrome DevTools.json";
+
 
 interface CodeEditorProps {
     value: string;
     language: string;
-    theme: "vs-dark" | "light";
+    theme: "monokai" | "chrome";
     onChange: (value: string | undefined) => void;
 }
 
 const CodeEditor: React.FC<CodeEditorProps> = ({ value, language, theme, onChange }) => {
+    useEffect(() => {
+        loader.init().then((monaco) => {
+            monaco.editor.defineTheme(
+                "monokai", monokai as monacoEditor.editor.IStandaloneThemeData
+            );
+            monaco.editor.defineTheme(
+                "chrome", chrome as monacoEditor.editor.IStandaloneThemeData
+            );
+        });
+    }, []);
+
     return (
-        <MonacoEditor
+        <Editor
             height="70vh"
             language={ language }
-            value={value}
+            value={ value }
             theme={ theme }
             onChange={ onChange }
             options={{
                 fontSize: 14,
-                minimap: { enabled: false },
-                scrollBeyondLastLine: false,
+                fontFamily: "Jetbrains Mono, Fira Code, monospace",
+                fontLigatures: true,
                 wordWrap: "on",
+                minimap: { enabled: false },
+                bracketPairColorization: { enabled: true },
+                autoClosingBrackets: "always",
+                autoIndent: "advanced",
+                cursorSmoothCaretAnimation: "on",
+                cursorBlinking: "expand",
+                renderLineHighlight: "all",
+                renderWhitespace: "selection",
+                guides: {
+                    indentation: true,
+                    bracketPairs: true,
+                    highlightActiveBracketPair: true,
+                },
+                glyphMargin: true,
+                scrollBeyondLastLine: false,
+                tabSize: 4,
             }}
+              
         />
     );
 };
